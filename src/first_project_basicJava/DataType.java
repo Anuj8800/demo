5f2dbf16a26daf714021906ae819bab8e1b27d70
Anuj8800 <anujmauriya30@gmail.com>
2023-01-29 15:14:07 +0530

    intial commit

---
+++ src/first_project_basicJava/DataType.java
@@ -0,0 +1,30 @@
+package first_project_basicJava;
+
+import java.util.Scanner; //
+
+public class DataType {
+      
+	//datatype variableName;
+	static byte a; static int b=5;  static float c; static double d; static long e; static char f; static String g;static boolean h;//decalration
+	public static void main(String[] args) {
+		
+		//ClassName  ObjectName = new Constructor (parameter)
+		Scanner sc =new Scanner(System.in); // object create
+		int obj1=sc.nextInt();
+		System.out.println(obj1);
+		
+		
+//		System.out.println(a );    // a  + space  + b + concatintion
+//		System.out.println(b);
+//		System.out.println(c);
+//		System.out.println(d);
+//		System.out.println(e);
+//		System.out.println(f);
+//		System.out.println(g);
+//		System.out.println(h);
+		
+		
+		
+	}
+
+}
